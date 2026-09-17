33ef800148a0c6924537fa23bbcbbfc9e902fa39
Kazi Omar Faruk <kaziomar144@gmail.com>
2026-09-17 14:36:53 +0600

    chore: clean up temporary deployment patch scripts

--- test_pil.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image, ImageDraw, ImageFont
-
-img = Image.new('RGBA', (1080, 1920), (0, 0, 0, 0))
-draw = ImageDraw.Draw(img)
-font = ImageFont.truetype("/var/www/python_engine/assets/fonts/HindSiliguri-Bold.ttf", 80)
-draw.text((100, 100), "পুনর্গঠনে ফিরে আসা", font=font, fill="white")
-img.save("test_bengali.png")
